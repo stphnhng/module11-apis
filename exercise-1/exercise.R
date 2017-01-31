8fba004b0d243d898fa749bef5d3c14a79a64701
Stephen Hung <hung.stephen.yh@gmail.com>
2017-01-31 14:36:31 -0800

    Finished Exercise 1 & 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,22 +1,41 @@
 ### Exercise 1 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
+library("httr")
+library("jsonlite")
+library('dplyr')
 
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
 
+unhcr.base.uri <- "http://data.unhcr.org"
+
 # For what years does the API have statistical data?
 
+response <- GET("http://data.unhcr.org/api/stats/time_series_years.json")
+body <- content(response,"text")
+years <- fromJSON(body)
+years
 
 # What is the "country code" for the "Syrian Arab Republic"?
 
+country.code.uri <- "/api/countries/list.json"
+country.code.response <- GET(paste0(unhcr.base.uri,country.code.uri))
+country.code.text <- content(country.code.response, "text")
+country.code.json <- fromJSON(country.code.text)
+country.code.frame <- flatten(country.code.json)
+syrian.arab.country.code <- country.code.frame$country_code[country.code.frame$name_en == "Syrian Arab Republic"]
+print(syrian.arab.country.code)
 
 # How many persons of concern from Syria applied for residence in the USA in 2013?
 # Hint: you'll need to use a query parameter
 # Use the `str()` function to print the data of interest
 # See http://www.unhcr.org/en-us/who-we-help.html for details on these terms
 
+query.params <- list(country_of_residence = "USA", country_of_origin = "SYR", year = 2013)
+response <- GET("http://data.unhcr.org/api/stats/persons_of_concern.json", query = query.params)
+usa.persons <- fromJSON(content(response,"text"))
+str(usa.persons)
 
 ## And this was only 2013...
 
@@ -24,9 +43,15 @@
 # How many *refugees* from Syria settled the USA in all years in the data set (2000 through 2013)?
 # Hint: check out the "time series" end points
 
+query.params <- list(country_of_residence = "USA", country_of_origin = "SYR", population_type_code = "RF")
+response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json?", query = query.params)
+refugees.usa <- fromJSON(content(response,"text"))
+refugees.usa <- select(refugees.usa, year, usa = value)
 
 # Use the `plot()` function to plot the year vs. the value.
 # Add `type="o"` as a parameter to draw a line
+
+plot(refugees.usa, type="o")
 
 
 
@@ -35,7 +60,11 @@
 # Is it more or less than the USA? (Hint: join the tables and add a new column!)
 # Hint: To compare the values, you'll need to convert the data (which is a string) to a number; try using `as.numeric()`
 
-
+query.params <- list(country_of_residence = "TUR", country_of_origin = "SYR", population_type_code = "RF")
+response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json?", query = query.params)
+refugees.other <- fromJSON(content(response,"text"))
+refugees.other <- select(refugees.other, year, other = value)
+refugees.other
 
 ## Bonus (not in solution):
 # How many of the refugees in 2013 were children (between 0 and 4 years old)?
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,10 +1,11 @@
 ### Exercise 2 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
+library("httr")
+library("jsonlite")
 
 # Create a `base.uri` variable that holds the base uri. You wil then paste endpoints to this base.
-
+base.uri <- "https://api.spotify.com"
 
 
 ## As you may have noticed, it often takes multiple queries to retrieve the desired information.
@@ -14,20 +15,43 @@
 
 # Define a function `TopTrackSearch` that takes in an artist name as an argument,
 # and returns the top 10 tracks (in the US) by that artist
-
-
-
+TopTrackSearch <- function(artist) {
+  # Artist info
+  resource <- "/v1/search"
+  uri = paste0(base.uri, resource)
+  query.params <- list(q = artist, type = "artist")
+  
+  response <- GET(uri, query = query.params)
+  
+  artist.info <- fromJSON(content(response, "text"))
+  artist.id <- artist.info$artists$items$id[1]
+  
+  # Get albums
+  resource <- paste0("/v1/artists/",artist.id,"/top-tracks")
+  uri = paste0(base.uri, resource)
+  query.params <- list(country="US")
+  response <- GET(uri, query = query.params)
+  
+  top.tracks <- fromJSON(content(response, "text"))
+  
+  return(top.tracks$tracks)
+}
 
 # What are the top 10 tracks by Beyonce?
-
+top.tracks <- TopTrackSearch('Beyonce')
 
 # Use the `flatten` function to flatten the data.frame -- note what differs!
-
+top.tracks <- flatten(top.tracks)
 
 # Use the `save()` function to save the flattened data frame to a file `beyonce.Rdata`
-
+save(top.tracks, file="beyonce.Rdata")
 
 # Use your `dplyr` functions to get the number of the songs that appear on each album
+num.album <- top.tracks %>%
+  group_by(album.name) %>%
+  summarise(n = n()) %>%
+  arrange(-n)
+num.album
 
 
 
